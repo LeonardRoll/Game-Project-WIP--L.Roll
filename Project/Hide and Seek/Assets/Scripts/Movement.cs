974f12f0576ee38036deeb7b76d9d2c8bb8340a6
LeonardRoll <leonard_roll@yahoo.co.uk>
2015-06-18 19:02:22 +0100

    Movement Script

--- Project/Hide and Seek/Assets/Scripts/Movement.cs
+++ Project/Hide and Seek/Assets/Scripts/Movement.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
+	//Global Variables
+	public float speed = 0.5f; //this is the characters normal movement speed
+	Vector3 direction; //Vector to store the direction of which the character will move
 
 	// Use this for initialization
 	void Start () {
@@ -9,7 +12,18 @@ public class Movement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+		//Local Variables
+
+
+		/*switch(Input.inputString)
+		{
+			case "w":
+				Debug.Log("W was pressed");
+				break;
+			default:
+				break;
+				//do nothing
+		}*/
 	}
 }
