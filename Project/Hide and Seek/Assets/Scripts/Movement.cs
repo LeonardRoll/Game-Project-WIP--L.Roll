da0709eda99fab528acaf0b03c47ddea30ad0872
LeonardRoll <leonard_roll@yahoo.co.uk>
2015-06-27 16:25:16 +0100

    Revert "Character Movement"
    This reverts commit fe3e91278983bef17d600fd416307dfd8484199a.

--- Project/Hide and Seek/Assets/Scripts/Movement.cs
+++ Project/Hide and Seek/Assets/Scripts/Movement.cs
@@ -2,21 +2,28 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-	public float speed = 6.0f;
-	public float gravity = 20.0f;
+	//Global Variables
+	public float speed = 0.5f; //this is the characters normal movement speed
+	Vector3 direction; //Vector to store the direction of which the character will move
 
-	private Vector3 moveDirection = Vector3.zero;
-
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
 	// Update is called once per frame
-	void Update (){
-		CharacterController controller = GetComponent<CharacterController>();
-		if(controller.isGrounded){
-			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0,
-			                        Input.GetAxis("Vertical"));
-			moveDirection = transform.TransformDirection(moveDirection);
-			moveDirection *= speed;
-		}
-		moveDirection.y -= gravity * Time.deltaTime;
-		controller.Move(moveDirection * Time.deltaTime);
+	void FixedUpdate () {
+		//Local Variables
+
+
+		/*switch(Input.inputString)
+		{
+			case "w":
+				Debug.Log("W was pressed");
+				break;
+			default:
+				break;
+				//do nothing
+		}*/
 	}
 }
